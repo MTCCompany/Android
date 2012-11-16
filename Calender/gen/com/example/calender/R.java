a17354d47baaa67310e3d249108fcb2eaca3eab9
TSZ0331 <TSZ0331@sts20090013.ad.srl.intra>
2012-11-16 17:40:20 +0900

    p179まで実施

--- Calender/gen/com/example/calender/R.java
+++ Calender/gen/com/example/calender/R.java
@@ -10,40 +10,62 @@ package com.example.calender;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int blue=0x7f040005;
+        public static final int darkGray=0x7f040001;
+        public static final int gray=0x7f040002;
+        public static final int green=0x7f040006;
+        public static final int lightGray=0x7f040000;
+        public static final int red=0x7f040004;
+        public static final int white=0x7f040003;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int button1=0x7f070001;
-        public static final int button2=0x7f070002;
-        public static final int menu_settings=0x7f070005;
-        public static final int relativeLayout1=0x7f070000;
-        public static final int textView1=0x7f070003;
-        public static final int textView2=0x7f070004;
+        public static final int Friday=0x7f08000e;
+        public static final int Monday=0x7f08000a;
+        public static final int Saturday=0x7f08000f;
+        public static final int Sunday=0x7f080009;
+        public static final int Thursday=0x7f08000d;
+        public static final int Tuesday=0x7f08000b;
+        public static final int Wednesday=0x7f08000c;
+        public static final int dayOfMonth=0x7f080001;
+        public static final int gridView1=0x7f080010;
+        public static final int linearLayout1=0x7f080007;
+        public static final int menu_settings=0x7f080011;
+        public static final int nextMonth=0x7f080006;
+        public static final int prevMonth=0x7f080004;
+        public static final int relativeLayout1=0x7f080003;
+        public static final int schedule=0x7f080002;
+        public static final int tableLayout1=0x7f080000;
+        public static final int tableRow1=0x7f080008;
+        public static final int yearMonth=0x7f080005;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int datecell=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int TuesDay=0x7f040008;
-        public static final int app_name=0x7f040000;
-        public static final int friday=0x7f04000b;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int monday=0x7f040007;
-        public static final int nextMonth=0x7f040005;
-        public static final int prevMonth=0x7f040004;
-        public static final int saturday=0x7f04000c;
-        public static final int sunday=0x7f040006;
-        public static final int thursday=0x7f04000a;
-        public static final int title_activity_main=0x7f040003;
-        public static final int wednesday=0x7f040009;
+        public static final int app_name=0x7f050000;
+        public static final int friday=0x7f05000b;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int monday=0x7f050007;
+        public static final int nextMonth=0x7f050005;
+        public static final int prevMonth=0x7f050004;
+        public static final int saturday=0x7f05000c;
+        public static final int sunday=0x7f050006;
+        public static final int thursday=0x7f05000a;
+        public static final int title_activity_main=0x7f050003;
+        public static final int tuesday=0x7f050008;
+        public static final int wednesday=0x7f050009;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- Calender/src/com/example/calender/MainActivity.java
+++ Calender/src/com/example/calender/MainActivity.java
@@ -2,19 +2,70 @@ package com.example.calender;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.view.LayoutInflater;
 
 public class MainActivity extends Activity {
 
+	private static final int DAYS_OF_WEEK = 7;			// 1週間の日数
+	private GridView mGridView = null;					// GridViewのインスタンス
+	private DateCellAdapter mDateCellAdapter = null;	// DateCellAdapterのインスタンス
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        mGridView = (GridView)findViewById(R.id.gridView1);
+        // Gridカラム数を設定
+        mGridView.setNumColumns(DAYS_OF_WEEK);
+        // DateCellAdapterのインスタンスを作成
+        mDateCellAdapter = new DateCellAdapter(this);
+        // GridViewに「DataCellAdapter」をセット
+        mGridView.setAdapter(mDateCellAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    /**
+     *  DateCellAdapterクラス
+     */
+    public class DateCellAdapter extends BaseAdapter{
+    	private static final int NUM_ROWS = 6;							// 行数
+    	private static final int NUM_OF_CELLS = DAYS_OF_WEEK*NUM_ROWS;	// セル数
+    	private LayoutInflater mLayoutInflater = null;
+    	
+    	DateCellAdapter(Context context){
+    		// getSystemServiceでXmlからViewオブジェクトを取得する。
+    		mLayoutInflater = 
+    		 (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    	}
+    	public int getCount(){ return NUM_OF_CELLS;}
+    	public Object getItem(int arg0){ return null;}
+    	public long getItemId(int arg0){ return 0;}
+    	public View getView(int position,View convertView,ViewGroup parent){
+    		if(convertView == null){
+				convertView = mLayoutInflater.inflate(R.layout.datecell,null);
+			}
+    		//Viewの最小の高さを設定する
+    		convertView.setMinimumHeight(parent.getHeight()/NUM_ROWS-1);
+    		// 日付の欄に仮に現在のpositionを表示しておく
+    		TextView dayOfMonthVeiw = (TextView)convertView.findViewById(R.id.dayOfMonth);
+    		dayOfMonthVeiw.setText(""+position);
+    		// スケジュール欄にはSchedule+posionと表示する。
+    		TextView scheduleView = (TextView)convertView.findViewById(R.id.schedule);
+    		scheduleView.setText("Schedule" + position);
+  
+    		return convertView;
+    	}
+    }
+     
 }
